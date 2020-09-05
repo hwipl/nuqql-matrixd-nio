04ff3f7f370eded9c1b185abea97195c21f0dc1f
hwipl <33433250+hwipl@users.noreply.github.com>
2020-09-05 21:28:15 +0200

    add matrixd_nio.py
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ matrixd_nio.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Helper script for starting matrixd-nio
+"""
+
+import sys
+
+import nuqql_matrixd_nio.main
+import nuqql_matrixd_nio
+
+# start nuqql-matrixd-nio
+sys.exit(nuqql_matrixd_nio.main.main())
