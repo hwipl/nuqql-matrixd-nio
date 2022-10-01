3eb634107c408e2dc55fcadd10fd3c05884466ad
hwipl <33433250+hwipl@users.noreply.github.com>
2022-10-01 10:18:01 +0200

    update matrix-nio to v0.20.0
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -61,5 +61,5 @@ setup(
     },
     classifiers=CLASSIFIERS,
     python_requires='>=3.7',
-    install_requires=["nuqql-based~=0.3.0", "matrix-nio[e2e]~=0.19.0"],
+    install_requires=["nuqql-based~=0.3.0", "matrix-nio[e2e]~=0.20.0"],
 )
